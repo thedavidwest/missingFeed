0324fa5630cedd2599a9f6fa5e7daca79ba08e2c
David West <thedavidwest@gmail.com>
2016-03-14 21:41:22 -0500

    hide video index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(link: 'https://www.youtube.com/embed/FTQbiNvZqaY', artist: 'Toto', song: 'Africa')
+Video.create(link: 'https://www.youtube.com/embed/dQw4w9WgXcQ', artist: 'Rick Astley', song: 'Never Gonna Give You Up')
+Video.create(link: 'https://www.youtube.com/embed/5DrjnffvVSQ', artist: 'The Cure', song: 'A Night Like This')
+Video.create(link: 'https://www.youtube.com/embed/2W4-G9-SKZM', artist: 'Gloria', song: 'Talking Heads 'This Must Be The Place'')
+Video.create(link: 'https://www.youtube.com/embed/uBx-Zz7CAOc', artist: 'Gang of Youths', song: 'LCD Soundsystem 'All My Friends'')
